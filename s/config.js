19e0424d7097f739c4d3beb97130c6a707789d3b
lloir <gitspamdump@gmail.com>
2024-01-10 11:45:10 +0000

    Fix locations + multiple use of app.use

--- s/config.js
+++ s/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     dbConfig: {
-        host: 'db_ip',
-        user: 'your_db_user',
-        password: 'your_db_password',
-        database: 'your_db_name',
+        host: '127.0.0.1',
+        user: 'test',
+        password: 'test',
+        database: 'wiki',
     },
 };
