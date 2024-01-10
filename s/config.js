cb37d64774d9f93c7c94e877c2093a5a48250c52
THIS IS MY NAME <github@0x3d.uk>
2024-01-10 11:48:33 +0000

    Update config.js

--- s/config.js
+++ s/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     dbConfig: {
-        host: '127.0.0.1',
-        user: 'test',
-        password: 'test',
-        database: 'wiki',
+        host: '127.0.0.1', // Change to your DB location
+        user: 'test', // Change to your DB User
+        password: 'test', // Change to your DB USER PASS
+        database: 'wiki', // Set to your DB
     },
 };
